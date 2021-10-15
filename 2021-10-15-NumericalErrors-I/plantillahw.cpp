1edcd36f9507160c5e2d7918d94a7ad2e2b9ffed
Iluvatar <woquendo@gmail.com>
2021-10-15 12:59:31 -0500

    Added homework template

---
+++ 2021-10-15-NumericalErrors-I/plantillahw.cpp
@@ -0,0 +1,28 @@
+// calcular el sin(x = pi/3), usando la expansion matematica
+// sin(x, N) = \sum_{n=1}^{N} (-1)^{n-1} x^{2n-1}/(2n-1)!
+
+#include <iostream>
+#include <cmath>
+
+double mysin(double x, int N);
+
+int main()
+{
+    std::cout.setf(std::ios::scientific);
+    std::cout.precision(6);
+
+    const double x = M_PI/3;
+    const double exact = std::sin(x);
+
+    for(int NMAX = 1; NMAX <= 1000; NMAX++) {
+        double diff = std::fabs(mysin(x, NMAX) - exact)/exact;
+        std::cout << NMAX << "\t" << diff << "\n";
+    }
+
+    return 0;
+}
+
+double mysin(double x, int N)
+{
+    return 0;
+}
