0e077d6136e2dfa767753cc026c86274be96a730
Iluvatar <woquendo@gmail.com>
2021-11-29 10:16:46 -0500

    Added run weak scaling script

---
+++ 2021-12-01-IntroHPC/run_weakscaling.sh
@@ -0,0 +1,4 @@
+source $HOME/repos/spack/share/spack/setup_env.sh
+spack load blas
+g++ -fopenmp -O3 -I $CMAKE_PREFIX_PATH/include -L $CMAKE_PREFIX_PATH/lib eigen-matmul.cpp -DEIGEN_USE_BLAS -lopenblas -o eigen_blas.x
+parallel -j 1 'echo -n "{}  "; OMP_NUM_THREADS={} ./eigen_blas.x 4000 10 2>/dev//null' ::: 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16
