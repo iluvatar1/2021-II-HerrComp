978f4be14664231852be18e03990aaeb47c21950
Iluvatar <woquendo@gmail.com>
2022-01-14 12:06:29 -0500

    Added improved pingpong

--- 2022-01-14-MPI-SendRecv_exer/pingpong.cpp
+++ 2022-01-14-MPI-SendRecv_exer/pingpong.cpp
@@ -1,32 +1,42 @@
 #include "mpi.h"
 #include <iostream>
+#include <cstdlib>
+
+void pingpong(int reps, int pid, int np);
 
 int main(int argc, char **argv)
 {
   /* MPI Variables */
-  int dest, np, pid, src, tag, buffer;
-  MPI_Status status;
+  int np, pid;
 
   /* MPI setup */
   MPI_Init(&argc, &argv);
   MPI_Comm_size(MPI_COMM_WORLD, &np);
   MPI_Comm_rank(MPI_COMM_WORLD, &pid);
 
-  tag = 0;
-  if (0 == pid) { /* Master */
-    MPI_Recv(&buffer, 1, MPI_INT, 1, tag, MPI_COMM_WORLD, &status);
-    MPI_Send(&pid, 1, MPI_INT, 1, tag, MPI_COMM_WORLD);
-    std::cout << buffer << std::endl;
-  }
-  else { /* slaves only send */
-    dest = 0;
-    MPI_Recv(&buffer, 1, MPI_INT, 0, tag, MPI_COMM_WORLD, &status);
-    buffer += 99;
-    MPI_Send(&buffer, 1, MPI_INT, 0, tag, MPI_COMM_WORLD);
-  }
+  const int REPS = std::atoi(argv[1]);  
+  pingpong(REPS, pid, np);
 
   /* finish */
   MPI_Finalize();
 
   return 0;
+}
+
+void pingpong(int reps, int pid, int np)
+{
+  MPI_Status status;
+  int tag = 0, buffer = pid;
+  for(int ii = 0; ii < reps; ++ii) {
+    if (0 == pid) { /* Master */
+      MPI_Send(&buffer, 1, MPI_INT, 1, tag, MPI_COMM_WORLD);
+      MPI_Recv(&buffer, 1, MPI_INT, 1, tag, MPI_COMM_WORLD, &status);
+      std::cout << buffer << std::endl;
+    }
+    else { /* slaves only send */
+      MPI_Recv(&buffer, 1, MPI_INT, 0, tag, MPI_COMM_WORLD, &status);
+      buffer += 99;
+      MPI_Send(&buffer, 1, MPI_INT, 0, tag, MPI_COMM_WORLD);
+    }
+  }
 }
